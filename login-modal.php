fdde999cc3f125dfc11969e24fbac9961aa9c95a
eloBernard <160846426+eloBernard@users.noreply.github.com>
2024-06-12 15:22:27 +0200

    Add files via upload

---
+++ login-modal.php
@@ -0,0 +1,20 @@
+<!-- login-modal.php -->
+<div id="loginModal" class="modal">
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <h2>Login or Register</h2>
+        <!-- Your login/register form goes here -->
+        <!-- Example form -->
+        <form>
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username"><br><br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password"><br><br>
+            <input type="submit" value="Login">
+            <input type="submit" value="Register">
+        </form>
+    </div>
+</div>
+
+</body>
+</html>
